7c67e76a82c69cf269e3b60c6293a7851cc9054f
David McGregor <David.McGregor@hl.co.uk>
2022-10-26 21:53:53 +0100

    Adding first API call code

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const sum = (num1, num2) => {
+console.log("running")
 
-    return num1+num2;
+
+//? first parameter = first value (vegi), & second parameter = second value (api key)
+
+async function getRecipeData(diet, cuisine) {
+    let theUrl = `https://api.spoonacular.com/recipes/complexSearch?diet=${diet}&cuisine=${cuisine}&apiKey=0c4d5d44095645ea9087a149dd32da6d&includeNutrition=true.`
+    console.log(theUrl)
+    let response = await fetch(theUrl)
+
+    if(response.status == 200) {
+        let apiResult = await response.json();
+        console.log(apiResult)
+
+    }
+
 
 }
+
+
+
+getRecipeData("vegan", "chinese");
