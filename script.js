d33f36abcaf531c78d8f0cb8f7ac861482307a9e
David McGregor <David.McGregor@hl.co.uk>
2022-10-27 11:15:30 +0100

    added functionality to function getting data from API

--- script.js
+++ script.js
@@ -3,20 +3,82 @@ console.log("running")
 
 //? first parameter = first value (vegi), & second parameter = second value (api key)
 
-async function getRecipeData(diet, cuisine) {
-    let theUrl = `https://api.spoonacular.com/recipes/complexSearch?diet=${diet}&cuisine=${cuisine}&apiKey=0c4d5d44095645ea9087a149dd32da6d&includeNutrition=true.`
+async function getRecipeData(diet, cuisine, minCalories, maxCalories) {
+    let theUrl = `https://api.spoonacular.com/recipes/complexSearch?diet=${diet}&cuisine=${cuisine}&minCalories=${minCalories}&maxCalories=${maxCalories}&apiKey=0c4d5d44095645ea9087a149dd32da6d&includeNutrition=true.`
     console.log(theUrl)
     let response = await fetch(theUrl)
 
     if(response.status == 200) {
         let apiResult = await response.json();
+
         console.log(apiResult)
+        
+        // let imageUrl = apiResult.results[0].image;
+
+        // let idNum = apiResult.results[0].id;
+
 
     }
-
 
 }
 
 
 
-getRecipeData("vegan", "chinese");
+
+
+
+    getRecipeData("vegan", "chinese", 100, 800);
+
+    async function returnAll(diet, cuisine, minCalories, maxCalories) {
+        let theUrl = `https://api.spoonacular.com/recipes/complexSearch?diet=${diet}&cuisine=${cuisine}&minCalories=${minCalories}&maxCalories=${maxCalories}&apiKey=0c4d5d44095645ea9087a149dd32da6d&includeNutrition=true.`
+        let response = await fetch(theUrl)
+
+        if(response.status == 200) {
+            let apiResult = await response.json();
+
+
+
+
+
+            for(i = 0; i < apiResult.results.length; i++) {
+
+                let title = apiResult.results[i].title;
+                console.log(title)
+           
+                let imageUrl = apiResult.results[i].image;
+                console.log(imageUrl)
+
+                let idNum = apiResult.results[i].id;
+                console.log(idNum)
+
+
+                var div = document.createElement('div')
+                p = document.createElement("p")
+                p.innerHTML = apiResult.results[i].title
+
+                p2 = document.createElement("p")
+                p2.innerHTML = apiResult.results[i].id
+
+                const img = document.createElement("img")
+                img.src = apiResult.results[i].image
+
+                div.append(p, img, p2);
+            
+
+
+                
+
+                
+                // const para = document.getElementById('p-id');
+                // const node = document.createTextNode(`${apiResult.results[i].title}`)
+
+                // para.appendChild(node);
+
+
+            }
+
+        }
+
+}
+
+    returnAll("vegan", "italian", 100, 800)
