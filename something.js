4125aa8651541a4f4bdb7d156af57b7d16362ee4
07GAshog <113988763+07GAshog@users.noreply.github.com>
2022-10-28 09:17:48 +0100

    How searching will work in something.js

--- something.js
+++ something.js
@@ -1,6 +1,6 @@
 const mainSection = document.querySelector("section")
 const input = document.createElement("input")
-let list = []
+let list = [11111, 12100, 33310]
 async function getRecipeData(diet, cuisine) {
     let response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?diet=${diet}&cuisine=${cuisine}&apiKey=b2d0a2fd5e6c48b1a41fef1086ed3a7d&includeNutrition=true`)
         data = await response.json()
@@ -20,16 +20,59 @@ async function getRecipeData(diet, cuisine) {
             //Adding evertyhing to the "cards"
             div.append(h1, image, p)
             list.push(element.id)
-            mainSection.append(div)
+            
             a = a + 1
             }
             
         });
 }
 mainSection.append(input)
+input.classList.add("block")
+
+// input.addEventListener("input", (whateEverTheInputIs) => {
+//     console.log(whateEverTheInputIs)
+// https://picsum.photos/200/300
 
 
-input.addEventListener("Input", (enteredText) => {
-    console.log(list, "Im working")
+let string = ""
+
+// const img1 = document.createElement("img")
+// const img2 = document.createElement("img")
+// const img3 = document.createElement("img")
+
+// img1.setAttribute("id", "image-1")
+// img2.setAttribute("id", "image-2")
+// img3.setAttribute("id", "image-3")
+
+// img1.src = "https://picsum.photos/201/300"
+// img2.src = "https://picsum.photos/202/300"
+// img3.src = "https://picsum.photos/203/300"
+
+// mainSection.append(img1, img2, img3)
+
+input.addEventListener("input", (enteredText) => {
+    if (enteredText.inputType == "deleteContentBackward") {
+        string = string.slice(0, -1)
+    }else if(enteredText.inputType == "deleteSoftLineBackward") {
+        string = ""
+    } else {
+        string += enteredText.data
+    }
+
+    
+
+    // if (mainSection.getElementById("#image-1") && mainSection.getElementById("#image-2") && mainSection.getElementById("image-3")) {
+
+    // } else {
+        
+    // }
+    
+
+    list.forEach(element => {
+        if (string == element) {
+            mainSection.removeChild(img3)
+            mainSection.prepend(img3)
+        }
+    })
 })
-getRecipeData("vegan", "chinese")
+// getRecipeData("vegan", "chinese")
